0f87ea537d206de7e489fd3327abb6f947eb8185
Xamoudy <mohamudadan224@gmail.com>
2026-05-23 20:07:10 +0300

    last Done

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -1,10 +1,10 @@
-import React from 'react'; const Footer = () => { return ( <> 
-
-<div>
-    <p>This Rights has been reserved</p>
-    <p>May - 2026</p>
-</div>
-
-</> )}
-
-export default Footer
+import React from 'react'; const Footer = () => {
+     return ( 
+    <>
+        <footer>
+        <p>© Event Planner Organization. All rights reserved</p>
+        </footer>
+    </> 
+    );
+};
+    export default Footer;
